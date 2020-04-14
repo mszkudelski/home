84bc4a78487c2cdbdc42329eaf59cb5ba6a61bdf
Marek Szkudelski <marek.szkudelski@sonalake.com>
2020-04-14 20:42:36 +0200

    technologies section added

---
+++ src/constants/technologies.const.ts
@@ -0,0 +1,86 @@
+export interface Technology {
+  name: string;
+  description: string;
+  logo: string;
+  experience: number;
+}
+
+export const Technologies: Technology[] = [
+  {
+    name: 'Javascript',
+    description:
+      'Pomimo tego, że używam na codzień frameworków oraz bibliotek, to znam ten język bardzo dobrze. Zarówno w standardzie EcmaScript 2015+ jak i wcześniejszym (ES5).',
+    experience: 4.5,
+    logo: 'js-logo.png',
+  },
+  {
+    name: 'Typescript',
+    description:
+      'Korzystam z Typescript przy każdym projekcie zarówno w pracy jak i po godzinach. Znam i rozumiem bardziej zaawansowane koncepty, które dostarcza.',
+    experience: 4,
+    logo: 'ts-logo.png',
+  },
+  {
+    name: 'Angular',
+    description:
+      'Na codzień w pracy tworzę złożone aplikacje biznesowe z użyciem Angular. Cenię sobie w nim solidną architekturę, Dependency Injection, albo modularność.',
+    experience: 4,
+    logo: 'ng-logo.png',
+  },
+  {
+    name: 'RxJs',
+    description:
+      "Za początku używałem RxJs tylko w projektach z Angular'em, ale potem dostrzegłem wartość jaką daje deklaratywne podejście do asychroniczności.",
+    experience: 3.5,
+    logo: 'rxjs-logo.png',
+  },
+  {
+    name: 'NodeJs',
+    description:
+      "Komercyjnie miałem okazję tworzyć i rozwijać dwa projekt z backend'em w NodeJs, korzystając z ExpressJs.",
+    experience: 3,
+    logo: 'node-logo.png',
+  },
+  {
+    name: 'NestJs',
+    description:
+      "Ze względu na podobne cechy do Angular'a spodobało mi się to co oferuje NestJs po stronie backend'owej. W prywatnych projektach wykorzystuję ten framework.",
+    experience: 3,
+    logo: 'nest-logo.png',
+  },
+  {
+    name: 'Css',
+    description:
+      'Znam dobrze podejście RWD, mobile-first, umiem korzystać z flexbox, a na codzień wykorzystuję preprocesor Scss do stylowania.',
+    experience: 4,
+    logo: 'css3-logo.png',
+  },
+  {
+    name: 'HTML',
+    description:
+      'Jest to podstawowa umiejętność frontend developera, ale nie zapominam o odpowiedniej semantyce ze względu na SEO albo dostępność.',
+    experience: 4.5,
+    logo: 'html-logo.png',
+  },
+  {
+    name: 'Docker',
+    description:
+      'Znam podstawy koncepcyjnie jak i praktycznie. Umiem przygotować proste kontenery do swoich projektów.',
+    experience: 2,
+    logo: 'docker-logo.png',
+  },
+  {
+    name: 'Jenkins',
+    description:
+      "Na codzień w pracy używam Jenkins'a do CI oraz CD. Znam podstawy konfiguracji tego narzędzia",
+    experience: 2,
+    logo: 'jenkins-logo.png',
+  },
+  {
+    name: 'Git',
+    description:
+      "Dość dobrze znam ten system kontroli wersji. Jestem przekonania, że dość trudno jest wprowadzić nieodwracalne zmiany w projektach z Git'em.",
+    experience: 4,
+    logo: 'git-logo.png',
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,41 @@
-import './styles/index.scss';
-import './assets/img/zdjęcie1-min.jpg';
+import '../assets/styles/index.scss';
+import '../assets/img/zdjęcie1-min.jpg';
+import '../assets/img/js-logo.png';
+import { Technologies } from './constants/technologies.const';
 
-console.log('asd')
+function getExperienceDots(experience: number): string {
+  return new Array(Math.ceil(experience))
+    .fill(null)
+    .reduce(
+      (result, _, index) =>
+        result +
+        `<div class="experience__dot ${
+          index + 1 > experience ? 'experience__dot--half' : ''
+        }"></div>`,
+      '',
+    );
+}
+
+function getTechnologyTemplate(technologies) {
+  return technologies.reduce(
+    (html, technology) =>
+      html +
+      `
+    <div class="tech__card">
+        <img class="tech__img" src="${require(`../assets/img/${
+          technology.logo
+        }`)}" aria-hidden="true" alt="${technology.name}">
+        <div class="experience">
+            ${getExperienceDots(technology.experience)}
+        </div>
+        <div class="tech__description">
+            ${technology.description}
+        </div>
+    </div>`,
+    '',
+  );
+}
+
+document.querySelector('#tech').innerHTML = getTechnologyTemplate(Technologies);
+
+export {};
