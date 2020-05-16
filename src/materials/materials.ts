f5219c0f3b0befd8357021a4c40b961c70d29b72
Marek Szkudelski <marek.szkudelski@sonalake.com>
2020-05-16 15:45:55 +0200

    fetching blog posts template

--- src/materials/materials.ts
+++ src/materials/materials.ts
@@ -1,5 +1,12 @@
 import { MaterialsTemplate } from './materials.template';
+import '../../assets/styles/materials.scss';
 
 window['__routes'].materials = function(container: HTMLElement) {
   container.innerHTML = MaterialsTemplate;
+
+  const articlesElement = document.querySelector('#articles');
+
+  fetch('articles.html')
+    .then((response) => response.json())
+    .then((articlesList) => (articlesElement.innerHTML = articlesList));
 };
