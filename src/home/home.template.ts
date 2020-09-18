5bc1059e363e3b75a5e3a1d74b507324655c03d7
Marek Szkudelski <marek.szkudelski@sonalake.com>
2020-09-18 10:44:24 +0200

    mail changed

--- src/home/home.template.ts
+++ src/home/home.template.ts
@@ -94,7 +94,7 @@ export const HomeTemplate = (jobs: string, tech: string) => `
                     <div class="social__icon social__icon--mail">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
                     </div>
-                    <a rel="noopener" aria-label="Link do emaila" class="social__link" href="mailto:marek.szkudelski@gmail.com">marek.szkudelski@gmail.com</a>
+                    <a rel="noopener" aria-label="Link do emaila" class="social__link" href="mailto:marek@szkudelski.dev">marek@szkudelski.dev</a>
                 </div>
             </div>
         </div>
